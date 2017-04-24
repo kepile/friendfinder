f0ac3ad2a75b3afaf8e21f5d60b855fb14812873
Kathleen Pile <Kathleen Pile>
2017-04-23 23:48:11 -0400

    removed hard coding and add new glyph

--- server.js
+++ server.js
@@ -2,10 +2,9 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
-// var path = require("path");
+
 // var router = express.Router;
-// var api = require('./routing/api-routes.js');  
-// var html = require('./routing/html-routes.js');  
+
 // EXPRESS CONFIGURATION
 // This sets up the basic properties for our express server
 var app = express();
@@ -14,6 +13,7 @@ var PORT = process.env.PORT || 3000;
 
 // BodyParser makes it possible for our server to interpret data sent to it.
 // The code below is pretty standard.app.use(bodyParser.json());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
@@ -29,54 +29,8 @@ app.use(express.static('./public'));
 require("./app/routing/api-routes")(app);
 require("./app/routing/html-routes")(app);
 
-// app.use('/', index);  
-// app.use('/api', api);  
-// app.use('/html', html-routes.js); 
- 
-      
 
 
-// Routes
-// =============================================================
-
-// Basic route that sends the user first to the AJAX Page
-// app.get("/", function(req, res) {
-//   res.sendFile(path.join(__dirname, "view.html"));
-// });
-
-// app.get("/", function(req, res) {
-//   res.sendFile(path.join(__dirname, "home.html"));
-
-// });
-
-// // Search for Specific Character (or all characters) - provides JSON
-// app.get("/api/:characters?", function(req, res) {
-//   var chosen = req.params.characters;
-
-//   if (chosen) {
-//     console.log(chosen);
-
-//     for (var i = 0; i < characters.length; i++) {
-//       if (chosen === characters[i].routeName) {
-//        return res.json(characters[i]);
-//       }
-//     }
-//     return res.json(false);
-//   }
-//   return res.json(characters);
-// });
-
-// // Create New Characters - takes in JSON input
-// app.post("/api/new", function(req, res) {
-//   var newcharacter = req.body;
-//   newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
-
-//   console.log(newcharacter);
-
-//   characters.push(newcharacter);
-
-//   res.json(newcharacter);
-// });
 
 // Starts the server to begin listening
 // =============================================================
